7354b90a84b60d44d22d72f9d3747962705aca75
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-05-23 09:33:18 +0800

    Update membership.py

--- membership.py
+++ membership.py
@@ -9,22 +9,27 @@ import torch
 
 from anfis import AnfisNet
 
-
+"""
+    将标量值转变为一个高斯隶属函数类模型可识别的torch参数
+"""
 def _mk_param(val):
-    '''Make a torch parameter from a scalar value'''
     if isinstance(val, torch.Tensor):
+        # 获取可迭代的数据
         val = val.item()
     return torch.nn.Parameter(torch.tensor(val, dtype=torch.float))
 
-
+"""
+    高斯隶属函数类模型
+    初始化需要传入两个参数均值mu与标准差sigma
+"""
 class GaussMembFunc(torch.nn.Module):
-    '''
-        Gaussian membership functions, defined by two parameters:
-            mu, the mean (center)
-            sigma, the standard deviation.
-    '''
     def __init__(self, mu, sigma):
         super(GaussMembFunc, self).__init__()
+        """
+            将一个不可训练的类型Tensor转换成可以训练的类型参数
+            将参数与模型绑定, 相当于变成了模型的一部分
+            成为了模型中可以根据训练进行变化的参数
+        """
         self.register_parameter('mu', _mk_param(mu))
         self.register_parameter('sigma', _mk_param(sigma))
 
@@ -35,7 +40,13 @@ class GaussMembFunc(torch.nn.Module):
     def pretty(self):
         return 'GaussMembFunc {} {}'.format(self.mu, self.sigma)
 
-
+"""
+    make_gauss_mfs函数参数:
+        - 隶属函数的标准差(sigma)
+        - 均值列表(mu_list)
+    函数返回:
+        - 高斯隶属函数类模型列表
+"""
 def make_gauss_mfs(sigma, mu_list):
     '''Return a list of gaussian mfs, same sigma, list of means'''
     return [GaussMembFunc(mu, sigma) for mu in mu_list]
@@ -195,22 +206,35 @@ get_class_for = {n: globals()[n]
                            'TrapezoidalMembFunc',
                            ]}
 
-
+"""
+    make_anfis函数的参数:
+        - 网络的输入变量x
+        - 隶属函数的个数num_mfs
+        - 网络输出量的个数num_out
+        - 是否使用最小二乘混合方法的标识位hybrid
+"""
 def make_anfis(x, num_mfs=5, num_out=1, hybrid=True):
-    '''
-        Make an ANFIS model, auto-calculating the (Gaussian) MFs.
-        I need the x-vals to calculate a range and spread for the MFs.
-        Variables get named x0, x1, x2,... and y0, y1, y2 etc.
-    '''
+    # 获取输入量的个数
     num_invars = x.shape[1]
+    # 沿着x每列求最大值和最小值
     minvals, _ = torch.min(x, dim=0)
     maxvals, _ = torch.max(x, dim=0)
+    # 得到输入各个状态量的取值范围
     ranges = maxvals-minvals
     invars = []
     for i in range(num_invars):
+        # 计算高斯隶属函数的方差
         sigma = ranges[i] / num_mfs
         mulist = torch.linspace(minvals[i], maxvals[i], num_mfs).tolist()
+        """
+            invars = [
+            ['x0', [GaussMembFunc(), ...]], 
+            ['x1', [GaussMembFunc(), ...]], ...
+         ]
+        """
         invars.append(('x{}'.format(i), make_gauss_mfs(sigma, mulist)))
+    # outvars = ['y0', 'y1, ...]
     outvars = ['y{}'.format(i) for i in range(num_out)]
+    # 将 invars 和 outvars 作为参数传入 AnfisNet() 建立 ANFIS 网络
     model = AnfisNet('Simple classifier', invars, outvars, hybrid=hybrid)
     return model
